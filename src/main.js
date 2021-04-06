325634990b94e11f382e57dc4a31658ed4978dd3
Doeun Kim <doeun.kim@emotion.co.kr>
2021-04-06 13:33:01 +0900

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import firebase from 'firebase'
 
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCbBolI-lyDLuS9Hz5wsPGd89EwmlDzIvw",
+    authDomain: "file-using-status.firebaseapp.com",
+    projectId: "file-using-status",
+    storageBucket: "file-using-status.appspot.com",
+    messagingSenderId: "834410177546",
+    appId: "1:834410177546:web:114d06890211d9e3fdebf3",
+    measurementId: "G-J4B5VT3RPK"
+};
+
+firebase.initializeApp(firebaseConfig)
+  
 createApp(App).mount('#app')
